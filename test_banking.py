489901d5c92533ee15961af043904eaae74a7375
Dora Kemeny <dory1318@gmail.com>
2019-05-06 13:49:35 +0200

    test for deposit money into account

--- test_banking.py
+++ test_banking.py
@@ -8,5 +8,11 @@ class TestAccount(unittest.TestCase):
         balance = bank.current_balance
         self.assertEqual (balance, 0)
 
+    def test_deposit_money(self):
+        bank = Account()
+        deposit = Deposit(100)
+        bank.deposit_money(deposit.amount)
+        self.assertEqual (balance, 100)
+
 if __name__ == '__main__':
     unittest.main()
