79dc001141d77ecc6f96ccef2d6d754f56912076
Dora Kemeny <dory1318@gmail.com>
2019-05-06 13:45:19 +0200

    initialize deposit and withdraw classes

--- banking.py
+++ banking.py
@@ -1,12 +1,16 @@
+from datetime import datetime
+
 class Account():
     def __init__(self):
         self.current_balance = 0
 
 class Deposit():
-    def __init__(self):
-        pass
+    def __init__(self, amount):
+        self.amount = amount
+        self.date = datetime.now().strftime("%d/%m/%Y")
 
 
 class Withdraw():
-    def __init(self):
-        pass
+    def __init__(self, amount):
+        self.amount = amount
+        self.date = datetime.now().strftime("%d/%m/%Y")
