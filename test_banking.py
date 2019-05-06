4acd14919f3f0852332bb4461d3ffe644922ecf5
Dora Kemeny <dory1318@gmail.com>
2019-05-06 13:06:02 +0200

    testing with unittest and pytest

---
+++ test_banking.py
@@ -0,0 +1,12 @@
+
+from banking import Account
+import unittest
+
+class TestAccount(unittest.TestCase):
+    def test_account(self):
+        bank = Account()
+        balance = bank.current_balance
+        self.assertEqual (balance, 0)
+
+if __name__ == '__main__':
+    unittest.main()
