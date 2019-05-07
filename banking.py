88b5d1c1cd6632fd3500775031826abb9f6b7b52
Dora Kemeny <dory1318@gmail.com>
2019-05-07 14:37:20 +0200

    passing test for transaction history

--- banking.py
+++ banking.py
@@ -9,22 +9,26 @@ class Account():
         self.current_balance += amount
         self.puts_deposit_into_transaction_history(deposit)
 
-    def withdraw_money(self, amount):
+    def withdraw_money(self, amount, withdraw):
         self.current_balance -= amount
+        self.puts_withdraw_into_transaction_history(withdraw)
 
     def puts_deposit_into_transaction_history(self, deposit):
         self.transaction_history.append([f"{deposit.date}, 0, {deposit.amount}, {self.current_balance}"])
 
+    def puts_withdraw_into_transaction_history(self, withdraw):
+        self.transaction_history.append([f"{withdraw.date}, {withdraw.amount}, 0, {self.current_balance}"])
+
 class Deposit():
     def __init__(self, amount, date):
         self.amount = amount
-        self.date = datetime.now().strftime("%d/%m/%Y")
+        self.date = date
 
 
 class Withdraw():
     def __init__(self, amount, date):
         self.amount = amount
-        self.date = datetime.now().strftime("%d/%m/%Y")
+        self.date = date
 
 class CurrentTime():
     def __init__(self):
--- test_banking.py
+++ test_banking.py
@@ -21,20 +21,21 @@ class TestAccount(unittest.TestCase):
         deposit = Deposit(1000, '10/01/2012')
         bank.deposit_money(deposit.amount, deposit)
         withdraw = Withdraw(500, '14/01/2012')
-        bank.withdraw_money(withdraw.amount)
+        bank.withdraw_money(withdraw.amount, withdraw)
         balance = bank.current_balance
         self.assertEqual (balance, 500)
 
     def test_puts_deposit_into_transaction_history(self):
         bank = Account()
+        transaction_history = bank.transaction_history
         deposit = Deposit(1000, '10/01/2012')
         deposit2 = Deposit(2000, '13/01/2012')
         withdraw = Withdraw(500, '14/01/2012')
         bank.deposit_money(deposit.amount, deposit)
         bank.deposit_money(deposit2.amount, deposit2)
-        bank.withdraw_money(withdraw.amount)
-        transaction_history = bank.transaction_history
-        self.assertEqual (transaction_history, [['date, withdraw, deposit, balance'], ["10/01/2012, 0, 1000, 1000"], ["13/01/2012, 0, 2000, 3000"], ["14/01/2012, 500, 0, 2500"]])
+        bank.withdraw_money(withdraw.amount, withdraw)
+        # transaction_history = bank.transaction_history
+        self.assertEqual (transaction_history, [['date, withdraw, deposit, balance'], ['10/01/2012, 0, 1000, 1000'], ['13/01/2012, 0, 2000, 3000'], ['14/01/2012, 500, 0, 2500']])
 
 
 if __name__ == '__main__':
