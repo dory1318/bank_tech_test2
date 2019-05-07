10f4917024f3af4f9b468ce805dbd0bf272d8be3
Dora Kemeny <dory1318@gmail.com>
2019-05-07 14:31:42 +0200

    deposit into transaction history

--- banking.py
+++ banking.py
@@ -5,11 +5,15 @@ class Account():
         self.current_balance = 0
         self.transaction_history = [['date, withdraw, deposit, balance']]
 
-    def deposit_money(self, amount):
+    def deposit_money(self, amount, deposit):
         self.current_balance += amount
+        self.puts_deposit_into_transaction_history(deposit)
 
     def withdraw_money(self, amount):
         self.current_balance -= amount
+
+    def puts_deposit_into_transaction_history(self, deposit):
+        self.transaction_history.append([f"{deposit.date}, 0, {deposit.amount}, {self.current_balance}"])
 
 class Deposit():
     def __init__(self, amount, date):
--- test_banking.py
+++ test_banking.py
@@ -12,14 +12,14 @@ class TestAccount(unittest.TestCase):
     def test_deposit_money(self):
         bank = Account()
         deposit = Deposit(1000, '10/01/2012')
-        bank.deposit_money(deposit.amount)
+        bank.deposit_money(deposit.amount, deposit)
         balance = bank.current_balance
         self.assertEqual (balance, 1000)
 
     def test_withdraw_money(self):
         bank = Account()
         deposit = Deposit(1000, '10/01/2012')
-        bank.deposit_money(deposit.amount)
+        bank.deposit_money(deposit.amount, deposit)
         withdraw = Withdraw(500, '14/01/2012')
         bank.withdraw_money(withdraw.amount)
         balance = bank.current_balance
@@ -30,8 +30,8 @@ class TestAccount(unittest.TestCase):
         deposit = Deposit(1000, '10/01/2012')
         deposit2 = Deposit(2000, '13/01/2012')
         withdraw = Withdraw(500, '14/01/2012')
-        bank.deposit_money(deposit.amount)
-        bank.deposit_money(deposit2.amount)
+        bank.deposit_money(deposit.amount, deposit)
+        bank.deposit_money(deposit2.amount, deposit2)
         bank.withdraw_money(withdraw.amount)
         transaction_history = bank.transaction_history
         self.assertEqual (transaction_history, [['date, withdraw, deposit, balance'], ["10/01/2012, 0, 1000, 1000"], ["13/01/2012, 0, 2000, 3000"], ["14/01/2012, 500, 0, 2500"]])
